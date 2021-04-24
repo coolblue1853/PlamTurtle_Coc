20a56a7047beb9865f68780260a33dfd3312d4e7
coolblue25721853 <coolblue1853@gmail.com>
2021-04-24 16:31:50 +0900

    스탯설명추가

--- A Shadow Over the Water/Assets/Scripts/setstet!/setCharNext.cs
+++ A Shadow Over the Water/Assets/Scripts/setstet!/setCharNext.cs
@@ -10,6 +10,17 @@ public class setCharNext : MonoBehaviour
     public GameObject 힘설명;
     public GameObject 지능설명;
     public GameObject 민첩설명;
+    public GameObject 행운설명;
+
+    public GameObject 활력설명;
+    public GameObject 의지력설명;
+    public GameObject 마력설명;
+    public GameObject 체력설명;
+    public GameObject 정신력설명;
+    public GameObject 스트레스내성설명;
+
+
+
 
     // Start is called before the first frame update
     public void 힘설명등장()
@@ -18,12 +29,15 @@ public class setCharNext : MonoBehaviour
 
 
     }
+    public void 힘설명가림()
+    {
+        힘설명.SetActive(false);
+    }
 
     public void 지능설명가림()
     {
         지능설명.SetActive(false);
     }
-
 
     public void 지능설명등장()
     {
@@ -43,14 +57,72 @@ public class setCharNext : MonoBehaviour
 
 
     }
-
-    public void 힘설명가림()
+    public void 행운설명가림()
     {
-        힘설명.SetActive(false);
+        행운설명.SetActive(false);
+    }
+
+    public void 행운설명등장()
+    {
+        행운설명.SetActive(true);
     }
 
 
+    public void 활력설명가림()
+    {
+        활력설명.SetActive(false);
+    }
 
+    public void 활력설명등장()
+    {
+        활력설명.SetActive(true);
+    }
+
+    public void 의지력설명가림()
+    {
+        의지력설명.SetActive(false);
+    }
+
+    public void 의지력설명등장()
+    {
+        의지력설명.SetActive(true);
+    }
+    public void 마력설명가림()
+    {
+        마력설명.SetActive(false);
+    }
+
+    public void 마력설명등장()
+    {
+        마력설명.SetActive(true);
+    }
+    public void 체력설명가림()
+    {
+        체력설명.SetActive(false);
+    }
+
+    public void 체력설명등장()
+    {
+        체력설명.SetActive(true);
+    }
+    public void 정신력설명가림()
+    {
+        정신력설명.SetActive(false);
+    }
+
+    public void 정신력설명등장()
+    {
+        정신력설명.SetActive(true);
+    }
+    public void 스트레스내성설명가림()
+    {
+        스트레스내성설명.SetActive(false);
+    }
+
+    public void 스트레스내성설명등장()
+    {
+        스트레스내성설명.SetActive(true);
+    }
 
 
 
