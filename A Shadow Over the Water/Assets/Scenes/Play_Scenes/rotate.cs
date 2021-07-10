c1beb6a7670be2e714e1fe68c6e5abed52aeefc4
coolblue25721853 <coolblue1853@gmail.com>
2021-07-11 01:47:11 +0900

    날자변환 애니메이션 완료

---
+++ A Shadow Over the Water/Assets/Scenes/Play_Scenes/rotate.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    float timer;
+    int waitingTime;
+    int updownder;
+    int 넘 =  0;
+    public Animator UD;
+    public Animator Ten;
+    public GameObject ring1;
+    public GameObject ring2;
+    public GameObject ring3;
+    public GameObject ring4;
+    bool 촉수체커 = false;
+    public bool isRing1End= false;
+    public bool isRing2End = false;
+    public bool isRing3End = false;
+    public bool isRing4End = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0.0f;
+        waitingTime = 5;
+        넘 = 0;
+        촉수체커 = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {if(collision.tag == "ring1")
+        {
+            isRing1End = true;
+        }
+
+        if (collision.tag == "ring2")
+        {
+            isRing2End = true;
+        }
+        if (collision.tag == "ring3" )
+        {
+
+            updownder = 2;
+            isRing3End = true;
+
+            if(촉수체커 == true)
+            {
+                넘 = 3;
+                Ten.SetInteger("Ten", 넘);
+            }
+        }
+        if (collision.tag == "ring4")
+        {
+            isRing4End = true;
+        }
+
+    }
+
+
+    public float turnSpeed1 = 30;
+    public float turnSpeed2 = 40;
+    public float turnSpeed3 = 20;
+    public float turnSpeed4 = 30;
+
+    bool isover = false;
+    // Update is called once per frame
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+
+        if (timer > waitingTime && isover == false)
+        {
+            isover = true;
+            updownder = 1;
+            isRing1End = false;
+            isRing2End = false;
+            isRing3End = false;
+            isRing4End = false;
+            촉수체커 = true;
+
+
+        }
+
+
+        if (updownder == 0)
+        {
+            UD.SetInteger("upDowner", 0);
+        }
+
+        else if (updownder == 1)
+        {
+            UD.SetInteger("upDowner", 1);
+        }
+
+        else if (updownder == 2)
+        {
+            UD.SetInteger("upDowner", 2);
+        }
+
+        if(isRing1End == false)
+        {
+            ring1.transform.Rotate(Vector3.forward, turnSpeed1 * Time.deltaTime);
+        }
+        if (isRing2End == false)
+        {
+            ring2.transform.Rotate(Vector3.forward, turnSpeed2 * Time.deltaTime);
+        }
+        if (isRing3End == false)
+        {
+            ring3.transform.Rotate(Vector3.forward, turnSpeed3 * Time.deltaTime);
+        }
+        if (isRing4End == false)
+        {
+            ring4.transform.Rotate(Vector3.forward, turnSpeed4 * Time.deltaTime);
+        }
+    }
+}
---
+++ A Shadow Over the Water/Assets/Scenes/Play_Scenes/woodUpDown.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class woodUpDown : MonoBehaviour
+{
+    public Animator UD;
+    public Animator Ten;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+        
+    }
+    int updownder;
+    int num=0;
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            num = num + 1;
+
+            Ten.SetInteger("Ten", num);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if(updownder == 0)
+            {
+                updownder = 1;
+            }
+
+            else if (updownder == 1)
+            {
+                updownder = 2;
+            }
+            else if (updownder == 2)
+            {
+                updownder = 0;
+            }
+        }
+
+        if (updownder == 0)
+        {
+            UD.SetInteger("upDowner",0);
+        }
+
+        else if (updownder == 1)
+        {
+            UD.SetInteger("upDowner", 1);
+        }
+
+        else if (updownder == 2)
+        {
+            UD.SetInteger("upDowner", 2);
+        }
+    }
+}
